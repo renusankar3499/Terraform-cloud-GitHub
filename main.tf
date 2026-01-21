2d41d9de887ed04c0f920543950987b3dcc8fa69
renusankar3499 <renusankar3499@gmail.com>
2026-01-21 14:07:31 +0530

    Update main.tf update by renu

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.14.3" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,5 +15,5 @@ provider "aws" {
 
 resource "aws_instance" "Raminfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
+	instance_type = "t3.micro"
 }
